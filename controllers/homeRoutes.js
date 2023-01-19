1a41db8b69931a6d924d6a65dc68392ad2017b13
Kenna Fackrell <kennac28@gmail.com>
2023-01-18 19:46:09 -0700

    fixed routing and posting on profile page for new posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-   
+    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
       ],
     });
 
-   
+    // Serialize data so the template can read it
     const posts = postData.map((post) => post.get({ plain: true }));
 
-   
+    // Pass serialized data and session flag into template
     res.render('homepage', { 
       posts, 
       logged_in: req.session.logged_in 
@@ -49,7 +49,7 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-
+// Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
